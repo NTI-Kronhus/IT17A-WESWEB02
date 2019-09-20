3b1657afee217c5c200ee456b235f2a415bedea0
NiklasHj <niklas.hjelm@ga.ntig.se>
2019-09-20 10:06:51 +0200

    Updated with contact

--- NodeIntro/server.js
+++ NodeIntro/server.js
@@ -1,22 +1,36 @@
 const { createServer } = require("http");
 const { createReadStream } = require("fs");
+const { decode } = require("querystring");
 
 const sendFile = (response, status, type, filePath) => {
-  res.writeHead(status, { "Content-Type": type });
+  response.writeHead(status, { "Content-Type": type });
   createReadStream(filePath).pipe(response);
 };
 
-createServer((request, result) => {
+createServer((request, response) => {
+  if (request.method === "POST") {
+    let body = "";
+    request.on("data", data => {
+      body += data;
+    });
+    request.on("end", () => {
+      const { name, email, message } = decode(body);
+      console.log(`${name} (${email}): ${message}`);
+    });
+  }
+
   switch (request.url) {
     case "/":
-      return sendFile(result, 200, "text/html", "./home-page.html");
+      return sendFile(response, 200, "text/html", "./home-page.html");
+    case "/contact":
+      return sendFile(response, 200, "text/html", "./contact.html");
     case "/img/niklas.jpeg":
-      return sendFile(result, 200, "image/jpeg", "./niklas.jpeg");
+      return sendFile(response, 200, "image/jpeg", "./niklas.jpeg");
     case "/styles.css":
-      return sendFile(result, 200, "text/css", "./styles.css");
+      return sendFile(response, 200, "text/css", "./styles.css");
     default:
-      return sendFile(result, 200, "text/html", "./404.html");
+      return sendFile(response, 200, "text/html", "./404.html");
   }
 }).listen(3000);
 
-console.log("Niklas' personal website runnning on port 3000");
+console.log("Alex's personal website runnning on port 3000");
