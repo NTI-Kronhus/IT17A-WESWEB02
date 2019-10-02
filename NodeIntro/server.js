d5b0d953141658b3b2cb8cc9bfa933441fa6d707
NiklasHj <niklas.hjelm@ga.ntig.se>
2019-10-02 09:56:16 +0200

    Added mongodb functionaliyuy

--- NodeIntro/server.js
+++ NodeIntro/server.js
@@ -2,6 +2,9 @@ const { createServer } = require("http");
 const { createReadStream } = require("fs");
 const { decode } = require("querystring");
 
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
 const sendFile = (response, status, type, filePath) => {
   response.writeHead(status, { "Content-Type": type });
   createReadStream(filePath).pipe(response);
@@ -15,22 +18,47 @@ createServer((request, response) => {
     });
     request.on("end", () => {
       const { name, email, message } = decode(body);
+      MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db("test");
+        var query = { email: `${email}` };
+        dbo.collection("WESWEB").find(query).toArray(function (err, result) {
+          if (err) throw err;
+          console.log("DEBUG")
+          console.log(result);
+          if (result.length!=0) {
+            var newvalues = { $set: { name: `${name}`, message: `${message}` } };
+            dbo.collection("WESWEB").updateOne(query, newvalues, function (err, res) {
+              if (err) throw err;
+              console.log("1 document updated");
+              db.close();
+            });
+          } else {
+            var myobj = { name: `${name}`, email: `${email}`, message: `${message}` };
+            dbo.collection("WESWEB").insertOne(myobj, function (err, res) {
+              if (err) throw err;
+              console.log("1 document inserted");
+              db.close();
+            });
+          }
+        });
+      });
       console.log(`${name} (${email}): ${message}`);
     });
   }
 
   switch (request.url) {
     case "/":
-      return sendFile(response, 200, "text/html", "./home-page.html");
+      return sendFile(response, 200, "text/html", "./client/home-page.html");
     case "/contact":
-      return sendFile(response, 200, "text/html", "./contact.html");
+      return sendFile(response, 200, "text/html", "./client/contact.html");
     case "/img/niklas.jpeg":
-      return sendFile(response, 200, "image/jpeg", "./niklas.jpeg");
+      return sendFile(response, 200, "image/jpeg", "./client/niklas.jpeg");
     case "/styles.css":
-      return sendFile(response, 200, "text/css", "./styles.css");
+      return sendFile(response, 200, "text/css", "./client/styles.css");
     default:
-      return sendFile(response, 200, "text/html", "./404.html");
+      return sendFile(response, 200, "text/html", "./client/404.html");
   }
 }).listen(3000);
 
-console.log("Alex's personal website runnning on port 3000");
+console.log("Niklas' personal website runnning on port 3000");
